f4ec01f5a4c11461b0b0e7bb78b67e60befb7ce5
Ahmet Ozer <43071105+cagri32@users.noreply.github.com>
2024-01-16 12:02:16 -0500

    Time: 99 ms (23.13%), Space: 18 MB (7.59%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,21 +3,21 @@ class Solution:
         # Sliding Window Approach
         # Space O(n) Time O(n)
         # use set for finding duplicate 
-        charSet = set()
-        l = 0
-        result= 0 
+#         charSet = set()
+#         l = 0
+#         result= 0 
         
-        for r in range(len(s)):
-            # Remove all chars from left until we get rid of all duplicates
-            # and increment left pointer at the same time
-            while s[r] in charSet:
-                charSet.remove(s[l])
-                l += 1
-            # if there is no duplicate, add chars from right
-            charSet.add(s[r])
-            # calculate the max of current string and the max we have
-            result = max(result, r-l+1)
-        return result
+#         for r in range(len(s)):
+#             # Remove all chars from left until we get rid of all duplicates
+#             # and increment left pointer at the same time
+#             while s[r] in charSet:
+#                 charSet.remove(s[l])
+#                 l += 1
+#             # if there is no duplicate, add chars from right
+#             charSet.add(s[r])
+#             # calculate the max of current string and the max we have
+#             result = max(result, r-l+1)
+#         return result
         
         # First Approach
         # every char needs to be unique
@@ -31,16 +31,22 @@ class Solution:
         # I will make the string into a char array because maintaining the arrays might be easier
         # or maybe not. not sure
         
-        # maxVal = 0
-        # newStr = ""
-        # char_list = list(s)
-        # print(char_list)
-        # for i, c in enumerate(char_list):
-        #     if c!= ' ' and c not in newStr:
-        #         newStr += c
-        #     else:
-        #         maxVal = max(maxVal,len(newStr))
-        #         print(newStr)
-        #         newStr = char_list[i]
-        # return max(maxVal,len(newStr))
+        maxVal = 0
+        newStr = ""
+        char_list = list(s)
+        print(char_list)
+        for i, c in enumerate(char_list):
+            if c!= ' ' and c not in newStr:
+                newStr += c
+            else:
+                maxVal = max(maxVal,len(newStr))
+                newStr += c
+                print("before")
+                print(newStr)
+                while newStr.count(c) > 1:
+                    newStr = newStr[1:]
+                print("after")
+                print(newStr)
+                
+        return max(maxVal,len(newStr))
                     
