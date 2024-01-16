43a535cdf8cc97f47bf2f14735ac30001718692e
Ahmet Ozer <43071105+cagri32@users.noreply.github.com>
2024-01-16 16:40:22 -0500

    Time: 59 ms (33.90%), Space: 42.5 MB (74.28%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.js
+++ 0022-generate-parentheses/0022-generate-parentheses.js
@@ -6,32 +6,34 @@ var generateParenthesis = function(n) {
     // create a tree of possible strings
     // n=2 means there will be 2^2 chars
     
+    const stack = []
     const result = []
-    const dfs = (i, n, slate, oCount, cCount) => {
-        // backtracking case
-        if(oCount > n) return;
-        if(cCount > oCount) return;
-        
+    const dfs = (oCount, cCount) => {
+
         // base case
-        if(i === n*2){
-            result.push(slate.join(''))
+        if(oCount === cCount && oCount === n){
+            result.push(stack.join(''));
             return;
         }
         
         // dfs recursive case
         
         // add (
-        slate.push('(')
-        dfs(i+1, n, slate, oCount+1, cCount)
-        slate.pop()
+        if(oCount < n){
+            stack.push('(')
+            dfs(oCount+1, cCount)
+            stack.pop()
+        }
         
         // add )
-        slate.push(')')
-        dfs(i+1, n, slate, oCount, cCount+1)
-        slate.pop()
+        if(cCount < oCount){
+            stack.push(')')
+            dfs(oCount, cCount+1)
+            stack.pop()
+        }
     }
     
-    dfs(0,n,[],0,0)
+    dfs(0,0)
     return result;
     
 };
