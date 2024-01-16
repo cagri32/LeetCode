094120acfb713fb715494417950666abe8a4f87f
Ahmet Ozer <43071105+cagri32@users.noreply.github.com>
2024-01-16 13:57:31 -0500

    Time: 49 ms (96.53%), Space: 17.5 MB (11.15%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,19 +1,60 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        # if this was not a permutation, it would simply be a find substring question
-        # I believe it is a different version of that
-        # A sliding window makes sense in this case
-        # We start sliding a window with size len(s1)
+        # better solution with sliding window
         if len(s1) > len(s2) or len(s2) == 0:
             return False
+        s1Count = {}
+        windowCount = {}
         
-        left, right = 0, len(s1) - 1
-        s1Sorted = sorted(s1)
-        while right <= len(s2):
-            # now this is a palindrome question
-            if s1Sorted == sorted(s2[left:right+1]):
+        # Initialize frequencies for the first window in s2
+        for char in s1:
+            s1Count[char] = s1Count.get(char, 0) + 1
+        for char in s2[:len(s1)]:
+            windowCount[char] = windowCount.get(char, 0) + 1
+            
+        
+        left, right = 0, len(s1) -1
+        
+        while right < len(s2):
+            # now this is a anagram question
+            # Check if the current window is an anagram of s1
+            if s1Count == windowCount:
                 return True
-            else:
-                left += 1
-                right += 1
+            
+            # get the char at left pointer
+            left_char = s2[left]
+            # decrease its freq in window frequency since we are moving right
+            windowCount[left_char] -= 1
+            # if the new freq is 0, delete it
+            if windowCount[left_char] == 0:
+                del windowCount[left_char]
+            
+            # get the new right after sliding right by 1
+            right += 1
+            if right < len(s2):
+                # to add into window count, get the char at the right pointer
+                right_char = s2[right]
+                # add it to the window
+                windowCount[right_char] = windowCount.get(right_char, 0) + 1
+        
+            # get the new left after sliding right by 1
+            left += 1
         return False
+          # First Approach that works with n^2logn since I keep sorting in the loop
+#         # if this was not a permutation, it would simply be a find substring question
+#         # I believe it is a different version of that
+#         # A sliding window makes sense in this case
+#         # We start sliding a window with size len(s1)
+#         if len(s1) > len(s2) or len(s2) == 0:
+#             return False
+        
+#         left, right = 0, len(s1) - 1
+#         s1Sorted = sorted(s1)
+#         while right <= len(s2):
+#             # now this is a palindrome question
+#             if s1Sorted == sorted(s2[left:right+1]):
+#                 return True
+#             else:
+#                 left += 1
+#                 right += 1
+#         return False
