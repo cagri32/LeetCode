1897a714d483a5f67e0a4558fe18c80b6ad6043e
Ahmet Ozer <43071105+cagri32@users.noreply.github.com>
2024-01-17 11:07:04 -0500

    Time: 46 ms (87.46%), Space: 42.2 MB (32.28%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -6,23 +6,16 @@ var findMin = function(nums) {
     let result = nums[0];
     let l = 0, r = nums.length -1;
     
-    while (l <= r){
-        // If right number is bigger than left, we get the leftmost as minimum
-        if (nums[l] < nums[r]) {
-            result = Math.min(result, nums[l])
-            break;
-        }
-        
+    while (l < r){
         // Calculate middle number
-        let m = Math.floor((l + r) / 2)
-        // Get the minimum of the result and middle
-        result = Math.min(result, nums[m]);
+        let m = Math.floor((l + (r - l)/ 2))
+
         // if middle is bigger or equal to left, update left to mid+1
-        if(nums[m] >= nums[l]){
+        if(nums[m] > nums[r]){
             l = m + 1;
         } else { // if middle is smaller than left, update right to mid-1
-            r = m - 1;
+            r = m;
         }
     }
-    return result
+    return nums[l]
 };
