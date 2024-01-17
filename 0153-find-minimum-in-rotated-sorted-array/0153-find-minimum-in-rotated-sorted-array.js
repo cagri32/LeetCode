cb4e4a775a7fbf33b3370d4a79e3d0bd084d1d17
Ahmet Ozer <43071105+cagri32@users.noreply.github.com>
2024-01-17 11:07:40 -0500

    Time: 48 ms (81.81%), Space: 41.8 MB (65.76%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -10,10 +10,10 @@ var findMin = function(nums) {
         // Calculate middle number
         let m = Math.floor((l + (r - l)/ 2))
 
-        // if middle is bigger or equal to left, update left to mid+1
+        // if middle is bigger or equal to right, update left to mid+1
         if(nums[m] > nums[r]){
             l = m + 1;
-        } else { // if middle is smaller than left, update right to mid-1
+        } else { // if middle is smaller than right, update right to mid
             r = m;
         }
     }
