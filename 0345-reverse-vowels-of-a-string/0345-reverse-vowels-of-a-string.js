104311390ddf1a9d3a795ebb556a87d4354e59aa
Ahmet Ozer <43071105+cagri32@users.noreply.github.com>
2024-01-17 00:00:15 -0500

    Time: 70 ms (60.96%), Space: 47.5 MB (87.69%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
@@ -3,10 +3,9 @@
  * @return {string}
  */
 var reverseVowels = function(s) {
-    const vowelArr = new Set(['a','e','i','o','u','A','E','I','O','U']);
-    const chars = s.split(''); // Convert string to array for efficient manipulation
-
     let left = 0, right = s.length - 1;
+    const chars = s.split(''); // Convert string to array for efficient manipulation
+    const vowelArr = new Set(['a','e','i','o','u','A','E','I','O','U']);
 
     while (left < right) {
         if (!vowelArr.has(chars[left])) {left++}
