ebb3b9539d976f21bb02077a4e3a7acb3116c3ee
Ahmet Ozer <43071105+cagri32@users.noreply.github.com>
2024-01-17 22:09:31 -0500

    Time: 155 ms (96.64%), Space: 48.4 MB (16.81%) - LeetHub

---
+++ 0454-4sum-ii/0454-4sum-ii.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number[]} nums3
+ * @param {number[]} nums4
+ * @return {number}
+ */
+var fourSumCount = function(nums1, nums2, nums3, nums4) {
+  let sumMap = new Map();
+  let count = 0;
+  
+  // First populate sum Map with sums of pairs from nums 1 and nums2
+  // 
+  for (let num1 of nums1){
+    for( let num2 of nums2){
+      const sum = num1 + num2;
+      // Check if the sumAB key already exists in sumMap
+      // If it exists, get its current count; otherwise, default to 0
+      const currentCount = sumMap.get(sum) || 0;
+
+      // Increment the count by 1 and update the Map with the new count
+      sumMap.set(sum, currentCount + 1);    
+    }
+  }
+  
+    // Check for complement sums in arrays C and D
+    for (let num3 of nums3) {
+        for (let num4 of nums4) {
+            const complement = 0 - (num3 + num4);
+            count += sumMap.get(complement) || 0;
+        }
+    }
+  return count
+};
