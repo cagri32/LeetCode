61ce5d86a7d14880ee7446fd204fb5ae568e8e96
Ahmet Ozer <43071105+cagri32@users.noreply.github.com>
2024-01-22 12:49:15 -0500

    Time: 53 ms (61.35%), Space: 49 MB (5.22%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.js
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -4,20 +4,22 @@
  * @return {boolean}
  */
 var isSubsequence = function(s, t) {
+    // If s is longer than t, it cant be a subsequence
     if(s.length > t.length){ return false }
+    // create 2 pointers
     let p1 = 0, p2= 0;
     
+    // iterate through t and s 
     while(p2 < t.length){
+        // if the pointers show the same char, increase both pointers, 
+        // otherwise just move the pointer of t
         if(s[p1]===t[p2]){
             p1++;
         }
         p2++;
     }
-    
-    if(p1 === s.length){
-        return true
-    }
-    return false
+    // if all the chars in s are seen in t, return true
+    return p1 === s.length
     
     
 };
