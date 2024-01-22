77db8f42fcd5911a2585412b7fe3e42cef2c9f74
Ahmet Ozer <43071105+cagri32@users.noreply.github.com>
2024-01-22 13:14:17 -0500

    Time: 88 ms (27.28%), Space: 55 MB (5.01%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -3,12 +3,17 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
+    // Initialize a pointer l to keep track of the position for non-zero elements.
     let l = 0;
-    
-    for(let r = 0; r < nums.length; r++){
-        if(nums[r] !== 0){
-            [nums[l], nums[r]] = [nums[r], nums[l]];
-            l++
+
+    // Iterate through the array using pointer r.
+    for (let r = 0; r < nums.length; r++) {
+        // If the current element at nums[r] is not zero, perform a swap with nums[l].
+        if (nums[r] !== 0) {
+            [nums[r], nums[l]] = [nums[l], nums[r]];
+            
+            // Increment l to the next position for non-zero elements.
+            l++;
         }
     }
     return nums
