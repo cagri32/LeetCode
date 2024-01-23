810e9d916eeba826046373012833a627f29181d6
Ahmet Ozer <43071105+cagri32@users.noreply.github.com>
2024-01-23 11:39:01 -0500

    Time: 55 ms (74.89%), Space: 51.8 MB (5.49%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.js
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -5,17 +5,16 @@
  */
 var canPlaceFlowers = function(flowerbed, n) {
     // while loop will start from length 3
-  
+    let possible = 0;
     for(let i = 0; i < flowerbed.length; i++){
-        if(flowerbed[i] === 0 
-           && (i == 0 || flowerbed[i-1] === 0) 
-           && (i === flowerbed.length-1 || flowerbed[i+1] === 0) 
-          ){
-            flowerbed[i] = 1
-            n--; 
+        if(flowerbed[i] === 0 // check if current spot is empty
+           && (i == 0 || flowerbed[i-1] === 0) // check if previous spot is empty or null
+           && (i === flowerbed.length-1 || flowerbed[i+1] === 0) // check if previous spot is empty or null
+          ){ // then put a flower, 
+            possible++; 
             i++
         } 
     }
     
-    return n <= 0;
+    return possible >= n;
 };
