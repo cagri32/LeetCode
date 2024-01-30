04632f542c1593b1b7524f910cca53e402ecc6d6
Ahmet Ozer <43071105+cagri32@users.noreply.github.com>
2024-01-30 15:19:40 -0500

    Time: 54 ms (96.65%), Space: 51.6 MB (20.46%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -4,10 +4,13 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
+    // reduce array to dictionary of keys: numbers, values: count of numbers
     let hash = nums.reduce((acc, curr)=>{
-        acc[curr.toString()] ? acc[curr.toString()] += 1 : acc[curr.toString()] = 1
+        acc[curr.toString()] ? 
+            acc[curr.toString()] += 1 : 
+            acc[curr.toString()] = 1
         return acc
     },{})
-    
+    // then sort keys of the dict, slice the first k ones and return in an array
     return [...Object.keys(hash).sort((a,b)=> hash[b]-hash[a]).slice(0,k)]
 };
