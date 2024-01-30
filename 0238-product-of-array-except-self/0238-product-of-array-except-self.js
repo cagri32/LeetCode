f1d332bd8add5255d4bda262b760c94c788395e2
Ahmet Ozer <43071105+cagri32@users.noreply.github.com>
2024-01-30 15:51:25 -0500

    Time: 96 ms (34.40%), Space: 64 MB (5.12%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    let forward = nums.reduce((acc, curr)=> {
+        let lastItem = acc.length !== 0 ? acc[acc.length-1] : 1
+        if(acc.length !==nums.length) {acc.push(curr * lastItem)}
+        return acc
+    },[1])
+    let backward = nums.reverse().reduce((acc, curr)=> {
+        let lastItem = acc.length !== 0 ? acc[acc.length-1] : 1
+        if(acc.length !==nums.length) {acc.push(curr * lastItem)}
+        return acc
+    },[1])
+    return backward.reverse().map((value, index) => value * forward[index]);
+};
